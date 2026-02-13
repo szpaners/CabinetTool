b65677daad45e313f2ac02547664f950a76e216d
Rafal <szpanerq@gmail.com>
2026-02-13 11:49:07 +0100

    Inputbox wymiarów

--- lib/cabinet_builder.rb
+++ lib/cabinet_builder.rb
@@ -117,25 +117,30 @@ module CabinetBuilder
   class Cabinet
     attr_reader :group, :entities
 
-    def initialize
+    def initialize(width, height, depth, panel_thickness, back_thickness)
       @model = Sketchup.active_model
       @entities = @model.active_entities
       @group = @entities.add_group
       @cabinet_entities = @group.entities
+      @width = width
+      @height = height
+      @depth = depth
+      @panel_thickness = panel_thickness
+      @back_thickness = back_thickness
     end
 
     def draw_bottom_panel
       bottom_x1 = 0
       bottom_y1 = 0
       bottom_z1 = 0
-      bottom_x2 = CabinetDimensions::CABINET_WIDTH
+      bottom_x2 = @width
       bottom_y2 = 0
       bottom_z2 = 0
-      bottom_x3 = CabinetDimensions::CABINET_WIDTH
-      bottom_y3 = CabinetDimensions::CABINET_DEPTH - CabinetDimensions::PANEL_THICKNESS_BACK
+      bottom_x3 = @width
+      bottom_y3 = @depth - @back_thickness
       bottom_z3 = 0
       bottom_x4 = 0
-      bottom_y4 = CabinetDimensions::CABINET_DEPTH - CabinetDimensions::PANEL_THICKNESS_BACK
+      bottom_y4 = @depth - @back_thickness
       bottom_z4 = 0
 
       bottom_point = [bottom_x1, bottom_y1, bottom_z1]
@@ -146,24 +151,24 @@ module CabinetBuilder
         [bottom_x4, bottom_y4, bottom_z4]
       ]
 
-      bottom_panel = BottomPanel.new(@cabinet_entities, Sketchup::Color.new(139, 90, 43), CabinetDimensions::PANEL_THICKNESS)
+      bottom_panel = BottomPanel.new(@cabinet_entities, Sketchup::Color.new(139, 90, 43), @panel_thickness)
       bottom_panel.create_face(bottom_rectangle)
-      bottom_panel.pushpull(-CabinetDimensions::PANEL_THICKNESS)
+      bottom_panel.pushpull(-@panel_thickness)
     end
 
-    def draw_top_panel
+def draw_top_panel
       top_x1 = 0
       top_y1 = 0
-      top_z1 = CabinetDimensions::CABINET_HEIGHT - CabinetDimensions::PANEL_THICKNESS
-      top_x2 = CabinetDimensions::CABINET_WIDTH
+      top_z1 = @height - @panel_thickness
+      top_x2 = @width
       top_y2 = 0
-      top_z2 = CabinetDimensions::CABINET_HEIGHT - CabinetDimensions::PANEL_THICKNESS
-      top_x3 = CabinetDimensions::CABINET_WIDTH
-      top_y3 = CabinetDimensions::CABINET_DEPTH - CabinetDimensions::PANEL_THICKNESS_BACK
-      top_z3 = CabinetDimensions::CABINET_HEIGHT - CabinetDimensions::PANEL_THICKNESS
+      top_z2 = @height - @panel_thickness
+      top_x3 = @width
+      top_y3 = @depth - @back_thickness
+      top_z3 = @height - @panel_thickness
       top_x4 = 0
-      top_y4 = CabinetDimensions::CABINET_DEPTH - CabinetDimensions::PANEL_THICKNESS_BACK
-      top_z4 = CabinetDimensions::CABINET_HEIGHT - CabinetDimensions::PANEL_THICKNESS
+      top_y4 = @depth - @back_thickness
+      top_z4 = @height - @panel_thickness
 
       top_point = [top_x1, top_y1, top_z1]
       top_rectangle = [
@@ -173,24 +178,24 @@ module CabinetBuilder
         [top_x4, top_y4, top_z4]
       ]
 
-      top_panel = TopPanel.new(@cabinet_entities, Sketchup::Color.new(139, 90, 43), CabinetDimensions::PANEL_THICKNESS)
+      top_panel = TopPanel.new(@cabinet_entities, Sketchup::Color.new(139, 90, 43), @panel_thickness)
       top_panel.create_face(top_rectangle)
-      top_panel.pushpull(CabinetDimensions::PANEL_THICKNESS)
+      top_panel.pushpull(@panel_thickness)
     end
 
-    def draw_left_panel
-      left_x1 = CabinetDimensions::PANEL_THICKNESS
+def draw_left_panel
+      left_x1 = @panel_thickness
       left_y1 = 0
-      left_z1 = CabinetDimensions::PANEL_THICKNESS
-      left_x2 = CabinetDimensions::PANEL_THICKNESS
+      left_z1 = @panel_thickness
+      left_x2 = @panel_thickness
       left_y2 = 0
-      left_z2 = CabinetDimensions::CABINET_HEIGHT - CabinetDimensions::PANEL_THICKNESS
-      left_x3 = CabinetDimensions::PANEL_THICKNESS
-      left_y3 = CabinetDimensions::CABINET_DEPTH - CabinetDimensions::PANEL_THICKNESS_BACK
-      left_z3 = CabinetDimensions::CABINET_HEIGHT - CabinetDimensions::PANEL_THICKNESS
-      left_x4 = CabinetDimensions::PANEL_THICKNESS
-      left_y4 = CabinetDimensions::CABINET_DEPTH - CabinetDimensions::PANEL_THICKNESS_BACK
-      left_z4 = CabinetDimensions::PANEL_THICKNESS
+      left_z2 = @height - @panel_thickness
+      left_x3 = @panel_thickness
+      left_y3 = @depth - @back_thickness
+      left_z3 = @height - @panel_thickness
+      left_x4 = @panel_thickness
+      left_y4 = @depth - @back_thickness
+      left_z4 = @panel_thickness
 
       left_point = [left_x1, left_y1, left_z1]
       left_rectangle = [
@@ -200,24 +205,24 @@ module CabinetBuilder
         [left_x4, left_y4, left_z4]
       ]
 
-      left_panel = LeftPanel.new(@cabinet_entities, Sketchup::Color.new(139, 90, 43), CabinetDimensions::PANEL_THICKNESS)
+      left_panel = LeftPanel.new(@cabinet_entities, Sketchup::Color.new(139, 90, 43), @panel_thickness)
       left_panel.create_face(left_rectangle)
-      left_panel.pushpull(CabinetDimensions::PANEL_THICKNESS)
+      left_panel.pushpull(@panel_thickness)
     end
 
-    def draw_right_panel
-      right_x1 = CabinetDimensions::CABINET_WIDTH
+def draw_right_panel
+      right_x1 = @width
       right_y1 = 0
-      right_z1 = CabinetDimensions::PANEL_THICKNESS
-      right_x2 = CabinetDimensions::CABINET_WIDTH
+      right_z1 = @panel_thickness
+      right_x2 = @width
       right_y2 = 0
-      right_z2 = CabinetDimensions::CABINET_HEIGHT - CabinetDimensions::PANEL_THICKNESS
-      right_x3 = CabinetDimensions::CABINET_WIDTH
-      right_y3 = CabinetDimensions::CABINET_DEPTH - CabinetDimensions::PANEL_THICKNESS_BACK
-      right_z3 = CabinetDimensions::CABINET_HEIGHT - CabinetDimensions::PANEL_THICKNESS
-      right_x4 = CabinetDimensions::CABINET_WIDTH
-      right_y4 = CabinetDimensions::CABINET_DEPTH - CabinetDimensions::PANEL_THICKNESS_BACK
-      right_z4 = CabinetDimensions::PANEL_THICKNESS
+      right_z2 = @height - @panel_thickness
+      right_x3 = @width
+      right_y3 = @depth - @back_thickness
+      right_z3 = @height - @panel_thickness
+      right_x4 = @width
+      right_y4 = @depth - @back_thickness
+      right_z4 = @panel_thickness
 
       right_point = [right_x1, right_y1, right_z1]
       right_rectangle = [
@@ -227,17 +232,17 @@ module CabinetBuilder
         [right_x4, right_y4, right_z4]
       ]
 
-      right_panel = RightPanel.new(@cabinet_entities, Sketchup::Color.new(139, 90, 43), CabinetDimensions::PANEL_THICKNESS)
+      right_panel = RightPanel.new(@cabinet_entities, Sketchup::Color.new(139, 90, 43), @panel_thickness)
       right_panel.create_face(right_rectangle)
-      right_panel.pushpull(CabinetDimensions::PANEL_THICKNESS)
+      right_panel.pushpull(@panel_thickness)
     end
 
-    def draw_back_panel
+def draw_back_panel
       back_x1 = 0
-      back_x2 = CabinetDimensions::CABINET_WIDTH
-      back_y = CabinetDimensions::CABINET_DEPTH
+      back_x2 = @width
+      back_y = @depth
       back_z1 = 0
-      back_z2 = CabinetDimensions::CABINET_HEIGHT
+      back_z2 = @height
 
       back_point = [back_x1, back_y, back_z1]
       back_rectangle = [
@@ -247,9 +252,9 @@ module CabinetBuilder
         [back_x1, back_y, back_z2]
       ]
 
-      back_panel = BackPanel.new(@cabinet_entities, Sketchup::Color.new(139, 90, 43), CabinetDimensions::PANEL_THICKNESS_BACK)
+      back_panel = BackPanel.new(@cabinet_entities, Sketchup::Color.new(139, 90, 43), @back_thickness)
       back_panel.create_face(back_rectangle)
-      back_panel.pushpull(CabinetDimensions::PANEL_THICKNESS_BACK)
+      back_panel.pushpull(@back_thickness)
     end
 
     def draw_cabinet
@@ -262,8 +267,14 @@ module CabinetBuilder
       puts "Kitchen cabinet created successfully!"
     end
 
-    def self.draw_cabinet
-      cabinet = Cabinet.new
+def self.draw_cabinet
+      width = UI.inputbox(["Podaj szerokość szafki (mm):"], ["600"]).first.to_f.mm
+      height = UI.inputbox(["Podaj wysokość szafki (mm):"], ["800"]).first.to_f.mm
+      depth = UI.inputbox(["Podaj głębokość szafki (mm):"], ["300"]).first.to_f.mm
+      panel_thickness = UI.inputbox(["Podaj grubość paneli (mm):"], ["18"]).first.to_f.mm
+      back_thickness = UI.inputbox(["Podaj grubość tylnej ścianki (mm):"], ["3"]).first.to_f.mm
+
+      cabinet = Cabinet.new(width, height, depth, panel_thickness, back_thickness)
       cabinet.draw_cabinet
     end
   end
